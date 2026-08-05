19494a9cb9358a93cb1557817f587eaa26418f9b
vikashittinhalli-wq <vikashittinhalli@icloud.com>
2026-08-05 09:18:26 +0800

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='homebound-v6-family-hero-save';
-const ASSETS=['./','./index.html','./styles.css?v=6','./app.js?v=7','./manifest.webmanifest','./assets/homebound-icon-180.png','./assets/homebound-icon-192.png','./assets/homebound-icon-512.png','./assets/home-hero-static.jpg','./assets/home-hero-family.svg'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
